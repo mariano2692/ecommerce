4a23345e007cb1119ac16b43265be57e3afd3626
Mariano Menendez <marianoalbertomenendez@gmail.com>
2026-03-03 20:37:42 -0300

    se agrega logs

--- microservices/common-exceptions/src/main/java/com/mmenendez/common_exceptions/GlobalExceptionHandler.java
+++ microservices/common-exceptions/src/main/java/com/mmenendez/common_exceptions/GlobalExceptionHandler.java
@@ -10,8 +10,11 @@ import org.springframework.web.bind.MethodArgumentNotValidException;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
 
+import lombok.extern.slf4j.Slf4j;
+
 @Component
 @RestControllerAdvice
+@Slf4j
 public class GlobalExceptionHandler {
 
         @ExceptionHandler(MethodArgumentNotValidException.class) 
@@ -23,7 +26,7 @@ public class GlobalExceptionHandler {
             var errorMessage = error.getDefaultMessage();
             errors.put(fieldName, errorMessage);
         });
-        
+        log.warn("Validation errors: {}", exception.toString());
         return ResponseEntity.status(HttpStatus.BAD_REQUEST).body(new ErrorResponse(errors)); 
     }
 
@@ -33,6 +36,7 @@ public class GlobalExceptionHandler {
         var fieldName = "message"; 
         var errorMessage = "An error has occurred. Please contact the administrator or try again later.";
         errors.put(fieldName, errorMessage); 
+        log.error("Error: {}", exception.toString());
         return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR).body(new ErrorResponse(errors)); 
     }
 
--- microservices/customer-microservice/src/main/java/com/mmenendez/microservices/customer_microservice/exceptions/CustomerExceptionHandler.java
+++ microservices/customer-microservice/src/main/java/com/mmenendez/microservices/customer_microservice/exceptions/CustomerExceptionHandler.java
@@ -12,8 +12,11 @@ import org.springframework.web.bind.annotation.RestControllerAdvice;
 import com.mmenendez.common_exceptions.ErrorResponse;
 import com.mmenendez.common_exceptions.GlobalExceptionHandler;
 
+import lombok.extern.slf4j.Slf4j;
+
 @RestControllerAdvice(basePackages = "com.mmenendez.microservices.customer_microservice")
 @Primary
+@Slf4j
 public class CustomerExceptionHandler extends GlobalExceptionHandler {
 
     @ExceptionHandler(CustomerNotFoundException.class)
@@ -23,7 +26,7 @@ public class CustomerExceptionHandler extends GlobalExceptionHandler {
         var errors = new HashMap<String, String>();
         var fieldName = "customer";
         errors.put(fieldName, exception.getMessage());
-
+        log.warn("Customer not found: {}" , exception.toString());
         return ResponseEntity.status(HttpStatus.BAD_REQUEST).body(new ErrorResponse(errors)); 
 
     }
